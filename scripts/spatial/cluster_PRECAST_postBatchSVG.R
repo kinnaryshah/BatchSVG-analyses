b14bdc4d3f8f00502bc959ca3ae1dfb5d69d174f
kinnaryshah <kinnaryshahh@gmail.com>
2025-09-08 11:03:35 -0400

    typo

--- scripts/spatial/cluster_PRECAST_postBatchSVG.R
+++ scripts/spatial/cluster_PRECAST_postBatchSVG.R
@@ -28,7 +28,7 @@ seuList <- unique(spe$subject) |>
             project="dACC")
     })
 
-svgs_after <- read.csv(file = here(data_name, "results", paste0(data_name, "_subject_10_5_filt_svgs.csv")))
+svgs_after <- read.csv(file = here(data_name, "results", paste0(data_name, "_subject_4_7_filt_svgs.csv")))
 
 set.seed(1)
 preobj <- CreatePRECASTObject(seuList = seuList, customGenelist = svgs_after$gene_id,
