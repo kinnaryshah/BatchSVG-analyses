3071de5325247d599dfc05a4563e7ff222a1cbca
kinnaryshah <kinnaryshahh@gmail.com>
2025-09-17 11:14:56 -0400

    use k=9 for NAc

--- scripts/spatial/cluster_PRECAST_postBatchSVG.R
+++ scripts/spatial/cluster_PRECAST_postBatchSVG.R
@@ -37,7 +37,7 @@ preobj@seulist
 
 PRECASTObj <- AddAdjList(preobj, platform = "Visium")
 PRECASTObj <- AddParSetting(PRECASTObj, Sigma_equal = FALSE,  maxIter = 20, verbose = TRUE)
-PRECASTObj <- PRECAST(PRECASTObj, K=8)
+PRECASTObj <- PRECAST(PRECASTObj, K=9)
 PRECASTObj <- SelectModel(PRECASTObj)
 seuInt <- IntegrateSpaData(PRECASTObj, species = "Human")
 
