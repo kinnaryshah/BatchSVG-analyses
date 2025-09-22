e9f0ec9dab8c35afa840905584bc27f1202ca240
Kinnary Shah <kinnaryshahh@gmail.com>
2025-09-22 13:54:31 -0400

    use harmony and bayesspace on n=3 mouse spleen

---
+++ scripts/spatial/cluster_harmony_BayesSpace_postBatchSVG.R
@@ -0,0 +1,41 @@
+setwd("Desktop/Research/BatchSVG-analyses/")
+library(here)
+library(Seurat)
+library(SpatialExperiment)
+library(scater)
+library(dplyr)
+library(tidyverse)
+library(harmony)
+library(scry)
+library(schex)
+
+setClassUnion("ExpData", c("matrix", "SummarizedExperiment"))
+
+data_name <- "SpatialBenchVisium_probe_based"
+load(file = here(data_name, "results", paste0(data_name, "_spe_qc.Rdata")))
+
+svgs_after <- read.csv(file = here(data_name, "results", paste0(data_name, "_sample_id_4_7_filt_svgs.csv")),row.names=1)
+
+set.seed(40)
+spe <- scater::runPCA(spe, ncomponents = 50,
+                      subset_row = rownames(svgs_after),
+                      BSPARAM = BiocSingular::RandomParam())
+
+spe <- RunHarmony(spe, "sample_id")
+spe <- runUMAP(spe, dimred = "HARMONY", name = "UMAP-HARMONY")
+
+colData(spe)$row <- spe$array_row
+colData(spe)$col <- spe$array_col
+
+metadata(spe)$BayesSpace.data <- list(platform = "Visium", is.enhanced = FALSE)
+
+k=7
+message("Running spatialCluster()")
+Sys.time()
+spe <- spatialCluster(spe, use.dimred = "HARMONY", q = k,nrep=10000)
+Sys.time()
+
+bayesSpace_name <- paste0("bayesSpace_captureArea_", k)
+colnames(colData(spe))[ncol(colData(spe))] <- bayesSpace_name
+
+save(spe, file = here(data_name, "results", paste0(data_name, "_spe_harmony_BayesSpace_postBatchSVG.Rdata")))
---
+++ scripts/spatial/cluster_harmony_BayesSpace_preBatchSVG.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=precast
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=20G
+#SBATCH --mail-type=END
+#SBATCH --mail-user=kinnaryshahh@gmail.com 
+
+echo "**** Job starts ****"
+date
+echo "**** SLURM info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+module load conda_R/4.5.x
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript cluster_harmony_BayesSpace_preBatchSVG.R
+
+echo "**** Job ends ****"
+date
