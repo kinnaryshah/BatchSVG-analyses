94c5ee3d02b181226697f83478e69f512c9fb502
Kinnary Shah <kinnaryshahh@gmail.com>
2025-10-06 13:41:09 -0400

    nextflow script for n=3 FFPE manual samples

---
+++ SpatialBenchVisium_709_710_713_FFPE_manual/download_FFPE_2_WT_710.sh
@@ -0,0 +1,30 @@
+cd preprocessed
+
+mkdir FFPE_2_WT_710
+cd FFPE_2_WT_710
+
+mkdir outs
+cd outs
+
+mkdir raw_feature_bc_matrix
+cd raw_feature_bc_matrix
+
+wget -O barcodes.tsv.gz "https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM8047nnn/GSM8047881/suppl/GSM8047881%5FFFPE%5F2%5FWT%5F710%5Fbarcodes%2Etsv%2Egz"
+wget -O features.tsv.gz "https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM8047nnn/GSM8047881/suppl/GSM8047881%5FFFPE%5F2%5FWT%5F710%5Ffeatures%2Etsv%2Egz"
+wget -O matrix.mtx.gz "https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM8047nnn/GSM8047881/suppl/GSM8047881%5FFFPE%5F2%5FWT%5F710%5Fmatrix%2Emtx%2Egz"
+
+cd ..
+mkdir spatial
+cd spatial
+
+wget -O tissue_lowres_image.png.gz "https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM8047nnn/GSM8047881/suppl/GSM8047881%5FFFPE%5F2%5FWT%5F710%5Ftissue%5Flowres%5Fimage%2Epng%2Egz"
+gunzip tissue_lowres_image.png.gz
+
+wget -O tissue_positions.csv.gz "https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM8047nnn/GSM8047881/suppl/GSM8047881%5FFFPE%5F2%5FWT%5F710%5Ftissue%5Fpositions%2Ecsv%2Egz"
+gunzip tissue_positions.csv.gz
+
+wget -O scalefactors_json.json.gz "https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM8047nnn/GSM8047881/suppl/GSM8047881%5FFFPE%5F2%5FWT%5F710%5Fscalefactors%5Fjson%2Ejson%2Egz"
+gunzip scalefactors_json.json.gz
+
+wget -O detected_tissue_image.jpg.gz "https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM8047nnn/GSM8047881/suppl/GSM8047881%5FFFPE%5F2%5FWT%5F710%5Fdetected%5Ftissue%5Fimage%2Ejpg%2Egz"
+gunzip detected_tissue_image.jpg.gz
---
+++ SpatialBenchVisium_709_710_713_FFPE_manual/download_FFPE_2_WT_713.sh
@@ -0,0 +1,30 @@
+cd preprocessed
+
+mkdir FFPE_2_WT_713
+cd FFPE_2_WT_713
+
+mkdir outs
+cd outs
+
+mkdir raw_feature_bc_matrix
+cd raw_feature_bc_matrix
+
+wget -O barcodes.tsv.gz "https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM8047nnn/GSM8047882/suppl/GSM8047882%5FFFPE%5F2%5FWT%5F713%5Fbarcodes%2Etsv%2Egz"
+wget -O features.tsv.gz "https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM8047nnn/GSM8047882/suppl/GSM8047882%5FFFPE%5F2%5FWT%5F713%5Ffeatures%2Etsv%2Egz"
+wget -O matrix.mtx.gz "https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM8047nnn/GSM8047882/suppl/GSM8047882%5FFFPE%5F2%5FWT%5F713%5Fmatrix%2Emtx%2Egz"
+
+cd ..
+mkdir spatial
+cd spatial
+
+wget -O tissue_lowres_image.png.gz "https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM8047nnn/GSM8047882/suppl/GSM8047882%5FFFPE%5F2%5FWT%5F713%5Ftissue%5Flowres%5Fimage%2Epng%2Egz"
+gunzip tissue_lowres_image.png.gz
+
+wget -O tissue_positions.csv.gz "https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM8047nnn/GSM8047882/suppl/GSM8047882%5FFFPE%5F2%5FWT%5F713%5Ftissue%5Fpositions%2Ecsv%2Egz"
+gunzip tissue_positions.csv.gz
+
+wget -O scalefactors_json.json.gz "https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM8047nnn/GSM8047882/suppl/GSM8047882%5FFFPE%5F2%5FWT%5F713%5Fscalefactors%5Fjson%2Ejson%2Egz"
+gunzip scalefactors_json.json.gz
+
+wget -O detected_tissue_image.jpg.gz "https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM8047nnn/GSM8047882/suppl/GSM8047882%5FFFPE%5F2%5FWT%5F713%5Fdetected%5Ftissue%5Fimage%2Ejpg%2Egz"
+gunzip detected_tissue_image.jpg.gz
